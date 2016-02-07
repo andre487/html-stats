0ea29bb964cf48acbf4cfe4d5083968f9084af75
andre487 <andre487@yandex-team.ru>
2016-02-07 03:06:55 +0300

    Implement stats counting

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,20 +1,19 @@
+var Assert = require('chai').assert;
 var Parser = require('../lib/parser');
-
-var assert = require('chai').assert;
 
 describe('parser', function () {
     describe('#parse()', function () {
         it('should parse correct raw HTML', function () {
-            var rawHtml = '<!DOCTYPE html><html><body><p>Some text';
+            var rawHtml = '<!DOCTYPE tags><tags><body><p>Some text';
 
             function check(dom) {
-                assert.isArray(dom);
-                assert.lengthOf(dom, 2);
+                Assert.isArray(dom);
+                Assert.lengthOf(dom, 2);
 
-                assert.deepPropertyVal(dom, '0.name', '!DOCTYPE');
-                assert.deepPropertyVal(dom, '1.name', 'html');
+                Assert.deepPropertyVal(dom, '0.name', '!DOCTYPE');
+                Assert.deepPropertyVal(dom, '1.name', 'tags');
 
-                assert.deepPropertyVal(dom, '1.children.0.children.0.children.0.data', 'Some text');
+                Assert.deepPropertyVal(dom, '1.children.0.children.0.children.0.data', 'Some text');
             }
 
             return Parser.parse(rawHtml).then(check);
@@ -24,11 +23,11 @@ describe('parser', function () {
             var rawHtml = '<div data-foo="bar">Some text</div>';
 
             function check(dom) {
-                assert.isArray(dom);
-                assert.lengthOf(dom, 1);
+                Assert.isArray(dom);
+                Assert.lengthOf(dom, 1);
 
-                assert.deepPropertyVal(dom, '0.data', 'div data-foo="bar"');
-                assert.deepPropertyVal(dom, '0.attribs.data-foo', 'bar');
+                Assert.deepPropertyVal(dom, '0.data', 'div data-foo="bar"');
+                Assert.deepPropertyVal(dom, '0.attribs.data-foo', 'bar');
             }
 
             return Parser.parse(rawHtml).then(check);
@@ -37,14 +36,14 @@ describe('parser', function () {
 
     describe('#flatten()', function () {
         it('should flatten DOM', function () {
-            var rawHtml = '<!DOCTYPE html><html><body><p data-foo="bar">Some text';
+            var rawHtml = '<!DOCTYPE tags><tags><body><p data-foo="bar">Some text';
 
             function check(nodesList) {
-                assert.deepPropertyVal(nodesList, '0.data', '!DOCTYPE html');
-                assert.deepPropertyVal(nodesList, '1.data', 'html');
-                assert.deepPropertyVal(nodesList, '2.data', 'body');
-                assert.deepPropertyVal(nodesList, '3.data', 'p data-foo="bar"');
-                assert.deepPropertyVal(nodesList, '4.data', 'Some text');
+                Assert.deepPropertyVal(nodesList, '0.data', '!DOCTYPE tags');
+                Assert.deepPropertyVal(nodesList, '1.data', 'tags');
+                Assert.deepPropertyVal(nodesList, '2.data', 'body');
+                Assert.deepPropertyVal(nodesList, '3.data', 'p data-foo="bar"');
+                Assert.deepPropertyVal(nodesList, '4.data', 'Some text');
             }
 
             return Parser.parse(rawHtml)
---
+++ test/stat-counter.test.js
@@ -0,0 +1,184 @@
+var Assert = require('chai').assert;
+
+var Parser = require('../lib/parser');
+var StatCounter = require('../lib/stat-counter');
+
+var countLength = require('../lib/count-utf8-length');
+
+describe('stat-counter', function () {
+    describe('#createDomStatsObject()', function () {
+        it('should provide object with correct structure', function () {
+            var stats = StatCounter.createDomStatsObject();
+
+            Assert.property(stats, 'total');
+            Assert.property(stats, 'tags');
+            Assert.property(stats, 'attributes');
+            Assert.property(stats, 'css');
+            Assert.property(stats, 'scripts');
+            Assert.property(stats, 'comments');
+            Assert.property(stats, 'text');
+        });
+    });
+
+    describe('#countDomStats()', function () {
+        describe('tags and attributes', function () {
+            it('should count directives sizes', function () {
+                var html = '<!DOCTYPE html>';
+
+                function check(stats) {
+                    Assert.propertyVal(stats, 'total', countLength(html));
+                    Assert.propertyVal(stats, 'directives', 15);
+
+                    Assert.equal(stats.directives, stats.total);
+                }
+
+                return Parser.parse(html)
+                    .then(Parser.flatten)
+                    .then(StatCounter.countDomStats)
+                    .then(check);
+            });
+
+            it('should count tag sizes', function () {
+                var html = '<p data-foo="bar">Some text</p>';
+
+                function check(stats) {
+                    Assert.propertyVal(stats, 'total', countLength(html));
+                    Assert.propertyVal(stats, 'attributes', 14);
+                    Assert.propertyVal(stats, 'tags', 8);
+                    Assert.propertyVal(stats, 'text', 9);
+
+                    Assert.equal(stats.attributes + stats.tags + stats.text, stats.total);
+                }
+
+                return Parser.parse(html)
+                    .then(Parser.flatten)
+                    .then(StatCounter.countDomStats)
+                    .then(check);
+            });
+
+            it('should count multiple tags sizes', function () {
+                var html = '<p data-foo="bar">Some text 1</p><p data-foo="bar">Some text 2</p>';
+
+                function check(stats) {
+                    Assert.propertyVal(stats, 'total', countLength(html));
+                    Assert.propertyVal(stats, 'attributes', 28);
+                    Assert.propertyVal(stats, 'tags', 16);
+                    Assert.propertyVal(stats, 'text', 22);
+
+                    Assert.equal(stats.attributes + stats.tags + stats.text, stats.total);
+                }
+
+                return Parser.parse(html)
+                    .then(Parser.flatten)
+                    .then(StatCounter.countDomStats)
+                    .then(check);
+            });
+
+            it('should count unicode sizes', function () {
+                var html = '<p>Текст в юникоде</p>';
+
+                function check(stats) {
+                    Assert.propertyVal(stats, 'total', countLength(html));
+                    Assert.propertyVal(stats, 'attributes', 0);
+                    Assert.propertyVal(stats, 'tags', 7);
+                    Assert.propertyVal(stats, 'text', 28);
+
+                    Assert.equal(stats.attributes + stats.tags + stats.text, stats.total);
+                }
+
+                return Parser.parse(html)
+                    .then(Parser.flatten)
+                    .then(StatCounter.countDomStats)
+                    .then(check);
+            });
+
+            it('should count comments sizes', function () {
+                var html = '<!-- Comment 1 --><p>Some text</p>';
+
+                function check(stats) {
+                    Assert.propertyVal(stats, 'total', countLength(html));
+                    Assert.propertyVal(stats, 'comments', 18);
+                    Assert.propertyVal(stats, 'tags', 7);
+                    Assert.propertyVal(stats, 'text', 9);
+
+                    Assert.equal(stats.comments + stats.tags + stats.text, stats.total);
+                }
+
+                return Parser.parse(html)
+                    .then(Parser.flatten)
+                    .then(StatCounter.countDomStats)
+                    .then(check);
+            });
+
+            it('should count script sizes', function () {
+                var html = '<script type="text/javascript">alert(1);</script>';
+
+                function check(stats) {
+                    Assert.propertyVal(stats, 'total', countLength(html));
+                    Assert.propertyVal(stats, 'attributes', 22);
+                    Assert.propertyVal(stats, 'tags', 18);
+                    Assert.propertyVal(stats, 'scripts', 9);
+
+                    Assert.equal(stats.attributes + stats.tags + stats.scripts, stats.total);
+                }
+
+                return Parser.parse(html)
+                    .then(Parser.flatten)
+                    .then(StatCounter.countDomStats)
+                    .then(check);
+            });
+
+            it('should count script[src] sizes', function () {
+                var html = '<script type="text/javascript" src="foo.js"></script>';
+
+                function check(stats) {
+                    Assert.propertyVal(stats, 'total', countLength(html));
+                    Assert.propertyVal(stats, 'attributes', 34);
+                    Assert.propertyVal(stats, 'tags', 19);
+
+                    Assert.equal(stats.attributes + stats.tags, stats.total);
+                }
+
+                return Parser.parse(html)
+                    .then(Parser.flatten)
+                    .then(StatCounter.countDomStats)
+                    .then(check);
+            });
+
+            it('should count styles sizes', function () {
+                var html = '<style type="text/css">body {font-size: 10px;}</style>';
+
+                function check(stats) {
+                    Assert.propertyVal(stats, 'total', countLength(html));
+                    Assert.propertyVal(stats, 'attributes', 15);
+                    Assert.propertyVal(stats, 'tags', 16);
+                    Assert.propertyVal(stats, 'css', 23);
+
+                    Assert.equal(stats.attributes + stats.tags + stats.css, stats.total);
+                }
+
+                return Parser.parse(html)
+                    .then(Parser.flatten)
+                    .then(StatCounter.countDomStats)
+                    .then(check);
+            });
+
+            it('should count void elements', function () {
+                var html = '<img src="foo.jpg" />';
+
+                function check(stats) {
+                    Assert.propertyVal(stats, 'total', countLength(html));
+                    Assert.propertyVal(stats, 'attributes', 13);
+                    Assert.propertyVal(stats, 'tags', 8);
+
+                    Assert.equal(stats.attributes + stats.tags, stats.total);
+                }
+
+                return Parser.parse(html)
+                    .then(Parser.flatten)
+                    .then(StatCounter.countDomStats)
+                    .then(check);
+            });
+        });
+    });
+});
